b63d799546f2c3e967aefd5f39322027b565edc4
Nikita Romaniuk <kelvium@yahoo.com>
2023-09-13 23:00:03 +0300

    generate simple HTML page

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-CFLAGS="-Wall -O0 -g -I/usr/lib/llvm/16/include"
-LDFLAGS="-O0 -g -L/usr/lib/llvm/16/lib -lclang"
+CFLAGS="-Wall -O2 -g -I/usr/lib/llvm/16/include"
+LDFLAGS="-O2 -g -L/usr/lib/llvm/16/lib -lclang"
 clang hash.c -o hash.o -c $CFLAGS
 clang main.c -o main.o -c $CFLAGS
 clang main.o hash.o -o main $LDFLAGS
